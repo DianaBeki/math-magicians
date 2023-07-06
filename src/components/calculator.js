26f9d302e589dd7a01fb03a7de3fb6d9541fb910
DianaBeki <dianabeki54@gmail.com>
2023-07-06 13:07:22 +0300

    Create components folder

---
+++ src/components/calculator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from './button';
+
+const Calculator = () => (
+  <div className="calculator">
+    <div className="display">0</div>
+    <div className="button-container">
+      <Button label="AC" />
+      <Button label="+/-" />
+      <Button label="%" />
+      <Button label="/" />
+      <Button label="7" />
+      <Button label="8" />
+      <Button label="9" />
+      <Button label="x" />
+      <Button label="4" />
+      <Button label="5" />
+      <Button label="6" />
+      <Button label="-" />
+      <Button label="1" />
+      <Button label="2" />
+      <Button label="3" />
+      <Button label="+" />
+      <Button label="0" />
+      <Button label="." />
+      <Button label="=" />
+    </div>
+  </div>
+);
+
+export default Calculator;
