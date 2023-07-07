52fb68e5cf86a15c85ab5c354b7f18d0141fed11
DianaBeki <dianabeki54@gmail.com>
2023-07-07 14:21:41 +0300

    Add classname to button to stle it using flexbox

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -8,22 +8,26 @@ const Calculator = () => (
       <Button label="AC" />
       <Button label="+/-" />
       <Button label="%" />
-      <Button label="/" />
+      <button type="button" className="orange">÷</button>
       <Button label="7" />
       <Button label="8" />
       <Button label="9" />
-      <Button label="x" />
+      <button type="button" className="orange">x</button>
       <Button label="4" />
       <Button label="5" />
       <Button label="6" />
-      <Button label="-" />
+      <button type="button" className="orange">-</button>
       <Button label="1" />
       <Button label="2" />
       <Button label="3" />
-      <Button label="+" />
-      <Button label="0" />
+      <button type="button" className="orange">+</button>
+    </div>
+    <div className="btn-row">
+      <div colSpan="2" className="button0">
+        <Button label="0" />
+      </div>
       <Button label="." />
-      <Button label="=" />
+      <button type="button" className="orange bg">=</button>
     </div>
   </div>
 );
