546db3fbaf9f86eab79d971bda1bce5c2eca34d3
DianaBeki <dianabeki54@gmail.com>
2023-07-07 18:51:06 +0300

    fetch API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Calculator from './components/calculator';
+import FetchQuotes from './components/display';
 
 const App = () => (
   <div className="section">
     <Calculator />
+    <FetchQuotes />
   </div>
 );
 export default App;
