be070e81f2e0f038c1e10ec4965cd550fc869f3f
Diana Beki <dianabeki54@gmail.com>
2023-07-21 05:48:36 -0400

    Add test to test quotes with jest

--- src/__test__/display.test.js
+++ src/__test__/display.test.js
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 import FetchQuotes from '../components/display';
+import  renderer from 'react-test-renderer';
 
-  <render />;
 describe('testing fetch quotes', () => {
   test('testing snapshots', () => {
-    const tree = render.create(<FetchQuotes />).toJSON;
+    const tree = renderer.create(<FetchQuotes />).toJSON; // Correctly call the render function with parentheses
     expect(tree).toMatchSnapshot();
   });
 });
